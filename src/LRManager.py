222bc25dab186117f5565f4b1d08c179b6c1b356
Andrew Lee <leeandrew135@gmail.com>
2025-12-08 20:16:48 -0500

    changes for testing

--- src/LRManager.py
+++ src/LRManager.py
@@ -87,19 +87,23 @@ if __name__ == "__main__":
 
     # TESTING THE MODEL INSTANCE
     model = manager.getModel()
-    # model.printSlopeIntercept()
-    # model.printErrorRanges()
+    model.printSlopeIntercept()
+    model.printErrorRanges()
 
-    printPrediction = model.predict(793)          # 793 - 497th ROW - INDEX [496]
-    printIndexPos = model.getIndexPosition(793)
-    # print(printPrediction)
-    # print(printIndexPos)
+    # printPrediction = model.predict(793)          # 793 - 497th ROW - INDEX [496]
+    # printIndexPos = model.getIndexPosition(793)
+    printPrediction = model.predict(1)          # 793 - 497th ROW - INDEX [496]
+    printIndexPos = model.getIndexPosition(1)
+    print(printPrediction)
+    print(printIndexPos)
 
     printIndexPos = model.getIndexPosition(794)
     # print(printIndexPos)
 
-    model.addIndex(399)
-    model.removeIndex(399)
+    # model.addIndex(399)
+    model.addIndex(1)
+    # model.removeIndex(399)
+    model.removeIndex(1)
 
     # testIndexes = [2024, 2022, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000]
 
